9d3a000c4f9e7421f2402825b72bad7b9a42d3f9
Jonathan Amadeus <jonathanamadeus95@gmail.com>
2024-06-12 21:31:54 +0700

    Add videoInfo.json

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  return res.render('index');
+  return res.send('Welcome to YT Downloader!');
 });
 
 const getInfo = (url, flags) =>
--- src/stash.js
+++ src/stash.js
@@ -286,3 +286,25 @@ app.get('/download/merge', async (req, res) => {
     console.log('Video download done.');
   });
 });
+
+app.get('/events', async function (req, res) {
+  console.log('Got /events');
+  res.set({
+    'Cache-Control': 'no-cache',
+    'Content-Type': 'text/event-stream',
+    Connection: 'keep-alive',
+  });
+  res.flushHeaders();
+
+  // Tell the client to retry every 10 seconds if connectivity is lost
+  res.write('retry: 10000\n\n');
+  let count = 0;
+
+  while (true) {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    console.log('Emit', ++count);
+    // Emit an SSE that contains the current 'count' as a string
+    res.write(`data: ${count}\n\n`);
+  }
+});
